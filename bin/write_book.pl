c879f6a2958ca95c6b5127576b7103ea4d5dc654
Leam Hall <leamhall@gmail.com>
2021-01-02 15:23:28 -0500

    Issue #2, added options, and -h.

--- bin/write_book.pl
+++ bin/write_book.pl
@@ -1,31 +1,106 @@
 #!/usr/bin/env perl
 
 # name:     write_book.pl
-# version:  0.0.2
-# date:     20210101
+# version:  0.0.4
+# date:     20210102
 # author:   Leam Hall
-# desc:     Write book from book and section objects.
+# desc:     Write book from section objects.
 
 ## CHANGELOG
 # 20210101  Work changes from Section.pm.
+# 20210102  Add default config, CLI options, and config file.
 
 use strict;
 use warnings;
+
+use Getopt::Long;
+use YAML::Tiny;
 
 use lib "lib";
 use Book;
 use Section;
 
-my $book_dir      = "/home/leam/lang/git/LeamHall/write_book_test";
-my $output_dir    = "$book_dir/book";
-my $sections_dir  = "$book_dir/sections";
-my $book_file     = "$output_dir/NavakSen_new.txt";
+### Config precedence:
+##  1. Command Line Options
+##  2. Config file variables
+##  3. Defaults
 
-my $book  = Book->new(
-  title   => "NavakSen",
-  author  => "Leam Hall",
+## Set up defaults
+my %configs  = (
+  book_dir    => '.',
+  output_dir  => 'book',
+  report      => 1,
+  report_dir  => 'reports',
+  section_dir => 'sections',
+  title       => "",
 );
 
+## Get options from config file
+my %file_configs;
+
+## Set up for GetOptions
+my $book_dir;
+my $config_file = 'book_config.yml';
+my $help;
+
+GetOptions(
+  "book_dir=s"    => \$book_dir,
+  "config_file=s" => \$config_file,
+  "help"          => \$help,
+);
+
+sub show_help {
+  print "Usage: $0 \n";
+  print "\t --book_dir <dir>       \n";
+  print "\t --config_file <file>   \n";
+  print "\t --help               This menu \n";
+  exit;
+}
+
+show_help() if $help;
+
+# Parse the config file, if there is one.
+if ( -f $config_file ) {
+  eval {
+    %file_configs = %{YAML::Tiny::LoadFile($config_file)};
+  }; 
+  if ( $@ ) {
+    print "Could not parse $config_file, exiting.\n";
+    exit;
+  }
+} else {
+  print "No config file found, exiting.\n";
+  exit;
+}
+  
+## Merge config file into %config, then CLI.
+%configs = ( %configs, %file_configs );
+unless ( $book_dir ) {
+  $book_dir = $configs{book_dir};
+}
+if ( ! -d $book_dir ) {
+  print "No directory $book_dir. Exiting.\n";
+  exit;
+}
+
+## Munge title into a reasonable file name.
+( my $file_name     = $configs{title} ) =~ s/[\W]/ /g;
+$file_name          = lc($file_name);
+$file_name          =~ s/\s*$//;
+$file_name          =~ s/\s+/_/g;
+$file_name          = $file_name . '.txt';
+
+# Set the main variables.
+my $output_dir      = "$book_dir/$configs{output_dir}";
+my $sections_dir    = "$book_dir/$configs{section_dir}";
+my $book_file       = "$output_dir/$file_name";
+
+
+## TODO: Is the book object needed?
+#   Maybe to list pre and post sections, to keep things in order.
+#   And to write the different types (text, LaTeX, XML, etc).
+
+## And away we go!
 open( my $file, '>', $book_file) or die "Can't open $book_file: $!";
 opendir( my $dir, $sections_dir) or die "Can't open $sections_dir: $!";
 
